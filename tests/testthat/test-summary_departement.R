25c3b4b2b536d771a9e54ce8fb0f359f88265565
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-02-21 11:17:04 +0100

    test de summary_departement

--- tests/testthat/test-summary_departement.R
+++ tests/testthat/test-summary_departement.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("summary_departement génère un résumé sans erreur", {
+  expect_no_error(summary_departement(df_Gers))
+})
+
+test_that("summary_departement échoue si le data frame contient une seule commune", {
+  expect_error(summary_departement(df_Nantes), "L'objet doit contenir plus d'une commune")
 })
