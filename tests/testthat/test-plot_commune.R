c5354f7def454d1039a365339254f01d3316f145
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-02-21 11:18:05 +0100

    test de plot_commune et departement

--- tests/testthat/test-plot_commune.R
+++ tests/testthat/test-plot_commune.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("plot_commune génère un graphique sans erreur", {
+  expect_no_error(plot_commune(df_Nantes))
+})
+
+test_that("plot_commune échoue si le dataframe n'a pas assez de colonnes", {
+  df_incomplet <- df_Nantes[, 1:5]  # Supprime des colonnes
+  expect_error(plot_commune(df_incomplet), "Le dataframe doit contenir 16 colonnes")
 })
--- tests/testthat/test-plot_departement.R
+++ tests/testthat/test-plot_departement.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("plot_departement génère un graphique sans erreur", {
+  expect_no_error(plot_departement(df_Gers))
+})
+
+test_that("plot_departement échoue si le dataframe n'a pas assez de colonnes", {
+  df_incomplet <- df_Gers[, 1:5]  # Supprime des colonnes
+  expect_error(plot_departement(df_incomplet), "Le dataframe doit contenir 16 colonnes")
 })
