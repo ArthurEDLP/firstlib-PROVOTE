99ba68d064d9b26bbcbda01a62eda224e3a04124
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-02-21 11:19:16 +0100

    test de creer_commune et departement

--- tests/testthat/test-creer_departement.R
+++ tests/testthat/test-creer_departement.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("creer_departement assigne correctement la classe Département", {
+  result <- creer_departement(df_Gers)
+  expect_s3_class(result, "Département")
+})
+
+test_that("creer_departement échoue si plusieurs départements sont présents", {
+  df_multi_departement <- elus_data
+  df_multi_departement$`Libellé du département`
+  expect_error(creer_departement(df_multi_departement), "L'objet doit contenir 1 département unique")
+})
+
+test_that("creer_departement échoue si une unique commune est présente", {
+  df_unique_commune <- df_Nantes
+  expect_error(creer_departement(df_unique_commune), "L'objet doit contenir plus d'une commune")
 })
